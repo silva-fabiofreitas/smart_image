3e0fe4ea7d114a6f80b876fbc7ce2d464cfdc056
fabio <fabio.f.silva.prestserv@petrobras.com.br>
2024-11-24 18:01:05 -0300

    refactor: Dividir resultados em colunas

--- frontend/pages/1_🤖_Structuring_with_AI.py
+++ frontend/pages/1_🤖_Structuring_with_AI.py
@@ -3,7 +3,17 @@ from pathlib import Path
 
 from graph.graph import app
 
+# Configuração inicial da página
+st.set_page_config(
+    page_title="Análise de Pareto",
+    page_icon="📊",
+    layout="wide",
+)
+
 st.title('Estruturar tabela com IA')
+st.write('Com a evolução do GPT-4, estruturar dados de tabelas a partir de imagens tornou-se mais acessível e '
+             'eficiente. Essa abordagem combina o poder do reconhecimento visual com a habilidade de modelagem de'
+             ' linguagem para transformar tabelas em formatos estruturados, como JSON ou modelos Pydantic.')
 
 onclick = st.button('Começar ?', type='primary')
 
@@ -14,12 +24,12 @@ if onclick:
         st.warning('Arquivo não encontrado')
         raise FileNotFoundError('Arquivo não encontrado')
 
-    for path in path.glob('*.*'):
-        with st.spinner('Processando...'):
-            res = app.invoke({'image_path': path, 'structure': True})
-        st.subheader(path.stem)
-        st.image(str(path))
-        st.dataframe(res['descriptions'][0])
+    cols = st.columns([6, 6])
+    for i, path in enumerate(path.glob('*.*')):
 
-
-
+        with cols[i % 2]:
+            with st.spinner('Processando...'):
+                res = app.invoke({'image_path': path, 'structure': True})
+            st.subheader(path.stem)
+            st.image(str(path))
+            st.dataframe(res['descriptions'][0])
